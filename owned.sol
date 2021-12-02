0517879ef673084066afe2d9473c5a11c03b89ac
sgnovo8 <88598099+sgnovo8@users.noreply.github.com>
2021-12-02 13:15:57 -0500

    Create owned.sol

---
+++ owned.sol
@@ -0,0 +1,20 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Owned {
+  address public owner;
+  
+  contstructor() {
+    owner = msg.sender;
+  }
+  
+  modifier onlyOwner {
+    require(msg.sender == owner);
+    _;
+  }
+  
+  function transferOwnership(address newOwner) public onlyOwner {
+    owner = newOwner;
+    
+  }
+
+}
